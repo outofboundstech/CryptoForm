295f1328c4555d29a28366f29c3637e16872e5f0
Marcel Oomens <mail@marceloomens.com>
2017-05-12 15:28:49 +0200

    Stubbing ElmMime
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- app/elm/ElmMime/Main.elm
+++ app/elm/ElmMime/Main.elm
@@ -1,6 +1,9 @@
 module ElmMime.Main exposing (Model, Part, serialize)
 
-{-| ElmMime types, model, model manipulation
+{-| ElmMime models mime email as (1) a cursor and (2) a stack of parts. The
+    cursor models the current part of the multipart mime message and has noy yet
+    been pushed onto the stack. Operations on the cursor include SetHeader and
+    UpdateBody. Operations on the stack include Push.
 -}
 
 
@@ -9,19 +12,72 @@ type alias Part =
   , body: String
   }
 
-type alias Model = List Part
+type alias Model =
+  { cursor: Part
+  , stack: List Part
+  }
 
-add_header : Part -> (String, String) -> Part
-add_header part header =
-  { part | headers = header :: part.headers}
+type Msg
+  = SetHeader (String, String)
+  | UpdateBody String
+  | Push Part
 
-set_body : Part -> String -> Part
-set_body part body =
-  { part | body = body }
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    SetHeader header ->
+      let
+        curr = model.cursor
+        next = { curr | headers = header :: curr.headers }
+      in
+        ( { model | cursor = next }, Cmd.none )
 
-add_part : Model -> Part -> Model
-add_part model part = part :: model
+    UpdateBody body ->
+      let
+        curr = model.cursor
+        next = { curr | body = body }
+      in
+        ( { model | cursor = next }, Cmd.none )
+
+    Push part ->
+      ( { model | stack = part :: model.stack }, Cmd.none )
 
 serialize : Model -> String
 serialize model =
-  "Hello, World!"
+  let
+    stack = model.cursor :: model.stack
+    frontier = mimeFrontier
+  in
+    List.foldr (\p str -> frontier ++ (String.trim <| serializePart p) ++ str)
+      frontier stack
+
+serializePart : Part -> String
+serializePart part =
+  serializeHeaders part.headers ++ "\n\n" ++ part.body
+
+serializeHeaders : List (String, String) -> String
+serializeHeaders headers =
+  -- I don't trim my header k and v here...
+  List.foldr (\(k, v) str -> (String.join ": " [k, v]) ++ "\n" ++ str)
+    "" headers
+
+-- Defaults
+
+mimeHeaders : List (String, String)
+mimeHeaders =
+  [ ("Mime-Version", "1.0")
+  ]
+
+plaintext : Part
+plaintext =
+  { headers = [("Content-Type", "text/plain")]
+  , body = ""
+  }
+
+
+-- Helpers functions
+
+mimeFrontier : String
+mimeFrontier =
+  -- Stubbing the frontier function
+  "\n\n4aUk7ggZLF9i6VUhHtrBCTP3AqArp8MH\n"
